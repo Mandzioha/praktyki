3e14cf67ff73fa72cda705715b5ddf187954ea72
Bartosz Pudo <pudobartosz95@gmail.com>
2023-05-08 12:38:58 +0200

    script added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let text_container = document.querySelector("h1");
+let button = document.querySelector("#welcome-button");
+let text = text_container.innerHTML;
+let clicked;
+button.addEventListener("click", ()=>{
+    if(clicked == true){
+        text_container.innerHTML = text;
+        clicked = false;
+    }else{
+        text_container.innerHTML ="Będzie pan zadowolony ;)";
+        clicked = true;  
+    }
+});
